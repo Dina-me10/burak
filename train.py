59decbf7a62c40d0bd1515cc4b3061d4096e501d
dina <dinaisakova10@gmail.com>
2026-05-25 21:55:04 +0900

    feat: MIT TASK M

--- train.py
+++ train.py
@@ -1,4 +1,18 @@
-# MIT TASK L
+print("MIT TASKLAR :)")
+
+print("MIT TASK M")
+
+
+def get_square_numbers(arr):
+    return [{"number": x, "square": x ** 2} for x in arr]
+
+
+print(get_square_numbers([1, 2, 3]))
+
+
+print("MIT TASK L")
+
+
 def reverse_sentence(sentence):
     return " ".join(word[::-1] for word in sentence.split(" "))
 
