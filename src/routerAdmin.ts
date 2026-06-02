d18eed68fcb2e72370d2f1e9654a9b22841e8f07
dina <dinaisakova10@gmail.com>
2026-06-02 11:36:25 +0500

    feat: create restaurant member related router get and post empoints

--- src/routerAdmin.ts
+++ src/routerAdmin.ts
@@ -2,10 +2,19 @@ import express from "express";
 const routerAdmin = express.Router();
 import restaurantController from "./controllers/restaurant.controller";
 
+/***RESTAURANT*****/
 routerAdmin.get("/", restaurantController.goHome);
 
+// Login
 routerAdmin.get("/login", restaurantController.getLogin);
+routerAdmin.post("/login", restaurantController.processLogin);
 
+// Signup
 routerAdmin.get("/signup", restaurantController.getSignup);
 
+routerAdmin.post("/signup", restaurantController.getSignup);
+
+/*****product ****** */
+
+/****user***** */
 export default routerAdmin;
