9c432b1b5f95698120cea7de157dbe26ed370ddb
dina <dinaisakova10@gmail.com>
2026-06-06 02:16:28 +0900

    feat: frontend related css, js and image creations

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -8,7 +8,8 @@ const restaurantController: T = {};
 
 restaurantController.goHome = (req: Request, res: Response) => {
   try {
-    res.send("Home Page");
+    res.render("home");
+    //send | json | redirect | end | render
   } catch (err) {
     console.log("Error, goHome:", err);
   }
@@ -16,7 +17,7 @@ restaurantController.goHome = (req: Request, res: Response) => {
 
 restaurantController.getSignup = (req: Request, res: Response) => {
   try {
-    res.send("Signup Page");
+    res.render("signup");
   } catch (err) {
     console.log("Error, getSignup:", err);
   }
@@ -24,7 +25,7 @@ restaurantController.getSignup = (req: Request, res: Response) => {
 
 restaurantController.getLogin = (req: Request, res: Response) => {
   try {
-    res.send("Login Page");
+    res.render("login");
   } catch (err) {
     console.log("Error, getLogin:", err);
   }
