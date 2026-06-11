5c7464f0656e27b19e3ba31ede5ca7cd898e5b7f
dina <dinaisakova10@gmail.com>
2026-06-11 14:17:40 +0900

    feat: upload files into server via multer

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -18,8 +18,9 @@ productController.getAllProducts = async (req: Request, res: Response) => {
 productController.createNewProduct = async (req: Request, res: Response) => {
   try {
     console.log("createNewProduct");
+    res.send("DONE");
   } catch (err) {
-    console.log("Error, gcreateNewProduct:", err);
+    console.log("Error, createNewProduct:", err);
     if (err instanceof Errors) res.status(err.code).json(err);
     else res.status(Errors.standard.code).json(Errors.standard);
   }
