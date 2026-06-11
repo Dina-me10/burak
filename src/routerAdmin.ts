e9dbbad139c18233404b060deaaa52f556d7a9d9
dina <dinaisakova10@gmail.com>
2026-06-11 11:11:57 +0900

    feat: locals variables integration

--- src/routerAdmin.ts
+++ src/routerAdmin.ts
@@ -3,29 +3,34 @@ const routerAdmin = express.Router();
 import restaurantController from "./controllers/restaurant.controller";
 import productController from "./controllers/product.controller";
 
-/***RESTAURANT*****/
+/* Restaurant */
 routerAdmin.get("/", restaurantController.goHome);
-
-// Login
-routerAdmin.get("/login", restaurantController.getLogin);
-routerAdmin.post("/login", restaurantController.processLogin);
-
-// Signup
-
-routerAdmin.get("/signup", restaurantController.getSignup);
-routerAdmin.post("/signup", restaurantController.processSignup);
+routerAdmin
+  .get("/login", restaurantController.getLogin)
+  .post("/login", restaurantController.processLogin);
+routerAdmin
+  .get("/signup", restaurantController.getSignup)
+  .post("/signup", restaurantController.processSignup);
 routerAdmin.get("/logout", restaurantController.logout);
-
 routerAdmin.get("/check-me", restaurantController.checkAuthSession);
 
-/*****product ****** */
-
+/** Product */
 routerAdmin.get(
   "/product/all",
   restaurantController.verifyRestaurant,
   productController.getAllProducts,
 );
-routerAdmin.post("/product/create", productController.createNewProduct);
-routerAdmin.post("/product/:id", productController.updateChosenProduct);
-/****user***** */
+routerAdmin.post(
+  "/product/create",
+  restaurantController.verifyRestaurant,
+  productController.createNewProduct,
+);
+routerAdmin.post(
+  "/product/:id",
+  restaurantController.verifyRestaurant,
+  productController.updateChosenProduct,
+);
+
+/** User */
+
 export default routerAdmin;
