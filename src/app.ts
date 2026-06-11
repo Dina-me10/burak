f2c5771666de1427e5f8641b16acb39effe0d681
dina <dinaisakova10@gmail.com>
2026-06-11 11:00:57 +0900

    feat: integration of locals variables

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import { MORGAN_FORMAT } from "./libs/config";
 
 import session from "express-session";
 import ConnectMongoDB from "connect-mongodb-session";
+import { T } from "./libs/types/common";
 
 const MongoDBStore = ConnectMongoDB(session);
 
@@ -33,6 +34,12 @@ app.use(
     resave: true, //10;30 auth => 13:30 12:00 => 15:00    saveUninitialized: true,
   }),
 );
+
+app.use(function (req, res, next) {
+  const sessionInstance = req.session as T;
+  res.locals.member = sessionInstance.member;
+  next();
+});
 
 /** 3-VIEWS **/
 app.set("views", path.join(__dirname, "views"));
