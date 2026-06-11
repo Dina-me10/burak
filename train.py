4177882343d46a1967dac36c0c2d992d4e5b013e
dina <dinaisakova10@gmail.com>
2026-06-11 14:21:15 +0900

    feat: MIT TASK S

--- train.py
+++ train.py
@@ -1,5 +1,17 @@
 print("MIT TASKLAR :)")
 
+print("MIT TASK S")
+
+
+def missingNumber(nums):
+    n = len(nums)
+    expected_sum = n * (n + 1) // 2
+    actual_sum = sum(nums)
+    return expected_sum - actual_sum
+
+
+print(missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]))
+
 print("MIT TASK R")
 
 
