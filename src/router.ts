f8ddcde5abbb97e4ff5a8acae40b23d82dd6f22f
MIRISKAKOVAMIRJALILKIZI MADINA <dinaisakova10@gmail.com>
2026-07-04 16:50:47 +0900

    feat: create middleware methods and develop logout process

--- src/router.ts
+++ src/router.ts
@@ -1,18 +1,19 @@
 import express from "express";
 const router = express.Router();
 import memberController from "./controllers/member.controller";
-      
-         //MEMBER 
 
+/** Member **/
 router.post("/member/login", memberController.login);
 router.post("/member/signup", memberController.signup);
+router.post(
+  "/member/logout",
+  memberController.verifyAuth,
+  memberController.logout
+);
 router.get("/member/detail", memberController.verifyAuth);
 
-//product 
+/** Product **/
 
-
-
-//order
-
+/** Order **/
 
 export default router;
