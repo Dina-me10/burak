67a9d61053f85d0aac8cc4bbf5e469deb37225f6
MIRISKAKOVAMIRJALILKIZI MADINA <dinaisakova10@gmail.com>
2026-07-07 04:48:50 +0900

    feat: develop getTopUsers rest API

--- src/router.ts
+++ src/router.ts
@@ -15,6 +15,8 @@ router.get("/member/detail", memberController.verifyAuth , memberController.getM
 
 
 router.post("/member/update", memberController.verifyAuth , uploader("members").single("memberImage"), memberController.updateMember);
+
+router.get("/member/top-users", memberController.getTopUsers);
 /** Product **/
 
 /** Order **/
