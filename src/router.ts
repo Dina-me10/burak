f7d92c17b83e0fe9211a95e764e6810bc5db48be
MIRISKAKOVAMIRJALILKIZI MADINA <dinaisakova10@gmail.com>
2026-07-09 04:44:59 +0900

    feat: develop getProducts business logic

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,7 @@ import express from "express";
 const router = express.Router();
 import memberController from "./controllers/member.controller";
 import uploader from "./libs/utils/uploader"
+import productController from "./controllers/product.controller";
 
 /** Member **/
 router.get("/member/restaurant", memberController.getRestaurant);
@@ -18,8 +19,9 @@ router.get("/member/detail", memberController.verifyAuth , memberController.getM
 router.post("/member/update", memberController.verifyAuth , uploader("members").single("memberImage"), memberController.updateMember);
 
 router.get("/member/top-users", memberController.getTopUsers);
-/** Product **/
 
+/** Product **/
+router.get("/product/all" ,  productController.getProducts);
 /** Order **/
 
 export default router;
